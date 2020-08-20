e463e8e384281a18f2e41b8c9da4e50af8a89794
Aleksey <Lelik13a@users.noreply.github.com>
2020-08-20 10:13:21 +0400

    Added peer monitoring

--- gluster-monitoring.pl
+++ gluster-monitoring.pl
@@ -34,6 +34,33 @@ print "\n\t]\n";
 print "}\n";
 }
 
+case "peers-discovery" {
+my $first = 1;
+
+print "{\n";
+print "\t\"data\":[\n\n";
+
+
+my $result = `gluster pool list | tail -n +2`;
+
+my @lines = split /\n/, $result;
+foreach my $l (@lines) {
+        my @stat = split /\s+/, $l;
+
+                print ",\n" if not $first;
+                $first = 0;
+
+                print "\t{\n";
+                print "\t\t\"{#UUID}\":\"$stat[0]\",\n";
+                print "\t\t\"{#HOSTNAME}\":\"$stat[1]\",\n";
+                print "\t\t\"{#STATE}\":\"$stat[2]\"\n";
+                print "\t}";
+}
+
+print "\n\t]\n";
+print "}\n";
+}
+
 case "status" {
 my $result = `gluster volume status $ARGV[1] detail --xml`;
         
@@ -47,5 +74,13 @@ my $result = `gluster volume info $ARGV[1] --xml`;
     print $result;
 
 }
+
+case "peers-status" {
+my $result = `gluster peer status --xml`;
+        
+    print $result;
+
+}
+
 }
 
